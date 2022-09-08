69b4d7af3a0bf3f32c0e3bb0f759c2ed35e4e9a2
Leandro.carlos <65824271+leandro-carlos@users.noreply.github.com>
2022-09-08 17:27:47 -0300

    up

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, StyleSheet, StatusBar } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, StatusBar, TextInput, Text } from 'react-native'
 import { colors, fonts } from './src/utils/theme'
 
 // Imports
@@ -7,6 +7,10 @@ import { colors, fonts } from './src/utils/theme'
 import Header from './src/components/Header'
 
 export default function App() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <View style={styles.container}>
       <StatusBar
@@ -17,7 +21,14 @@ export default function App() {
 
       <Header />
 
-    </View>
+      <TextInput onChangeText={setEmail} style={styles.btnLogin}></TextInput>
+      <TextInput onChangeText={setPassword} style={styles.btnLogin}></TextInput>
+
+      <Text style={styles.txtEmail}>
+        Seu email é {email}
+      </Text>
+
+    </View >
   )
 }
 
@@ -25,5 +36,17 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.background_primary,
     flex: 1
+  },
+  btnLogin: {
+    backgroundColor: colors.shape_dark,
+    width: '90%',
+    margin: 15,
+    color: 'white'
+  },
+  txtEmail: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 20,
+    color: colors.text,
   }
 })
